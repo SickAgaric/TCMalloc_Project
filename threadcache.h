a9a0e967cf9b5476d1c194cfc9ceae67418a8454
mochu823@yeah.net <mochu823@yeah.net>
2020-03-10 22:55:55 +0800

    a

--- threadcache.h
+++ threadcache.h
@@ -10,6 +10,10 @@ public:
 	void Deallocate(void* ptr, size_t size);
 	//从中心cache取内存
 	void* FetchFromCentralCache(size_t index);
+	//如果自由链表中对象超过一定长度
+	void ListLong(FreeList& freelist, size_t num, size_t size);
 private:
 	FreeList _freeLists[NFRER_LIST];
 };
+
+_declspec (thread) static Threadcache* tlsThread = nullptr;
